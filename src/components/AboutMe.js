d6bb309bf29d6d64d0c3edce7f99e01fca7b097f
William Saar <williamsaar@outlook.com>
2022-03-28 23:46:56 +0200

    about me page responsive and button bounce

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,10 +1,13 @@
 import "./AboutMe.css";
 
+import { ReactComponent as BeerLogo } from "../assets/icons/beer-svgrepo-com.svg";
+import { ReactComponent as DeveloperLogo } from "../assets/icons/web-development-svgrepo-com.svg";
+
 export default function AboutMe() {
   return (
     <div className="about-me-container">
       <div className="about-me-top">
-        <p className="element-text-about-me">{'< div className="aboutme" >'}</p>
+        <p className="element-text-about-me">{"< section >"}</p>
         <div>
           <h2 className="hero-title2">About me</h2>
           <div className="hero-ball">
@@ -15,13 +18,12 @@ export default function AboutMe() {
 
         <div className="about-me-text-container">
           <p>
-            Hi! I'm William! <br /> <br /> I'm a Front End developer based in
-            Gothenburg. I describe myself as a passionate and creative developer
-            who loves coding, design, and the web platform. <br /> <br /> Aside
-            from my job, I always strive to learn more and expand my knowledge
-            in developing. I often find myself spending way too much time
-            watching videos and reading about new smart solutions and projects.{" "}
-            <br />
+            I'm a Front End developer based in Gothenburg. I describe myself as
+            a passionate and creative developer who loves coding, design, and
+            the web platform. <br /> <br /> Aside from my job, I always strive
+            to learn more and expand my knowledge in developing. I often find
+            myself spending way too much time watching videos and reading about
+            new smart solutions and projects. <br />
             <br /> In my free time you can find me longboarding, traveling, in
             some local pub watching football or on tech meetups and conferences
             around Gothenburg.
@@ -31,6 +33,8 @@ export default function AboutMe() {
       <div className="cloud-container">
         <div class="cloud">
           <div className="cloud-circle" />
+          <BeerLogo className="beer-logo" fill="#555555" />
+          <DeveloperLogo className="developer-logo" fill="#555555" />
           <div className="cloud-circle2" />
         </div>
       </div>
