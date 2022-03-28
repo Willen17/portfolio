0f6d46f9650a4856574ebe5667346182e230d064
William Saar <williamsaar@outlook.com>
2022-03-28 21:18:56 +0200

    fixed responsive aboutme page

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,6 +4,7 @@ export default function AboutMe() {
   return (
     <div className="about-me-container">
       <div className="about-me-top">
+        <p className="element-text-about-me">{'< div className="aboutme" >'}</p>
         <div>
           <h2 className="hero-title2">About me</h2>
           <div className="hero-ball">
@@ -12,19 +13,28 @@ export default function AboutMe() {
           </div>
         </div>
 
-        <div>
-          Hi! I'm William! <br /> <br /> I'm a Front End developer based in
-          Gothenburg. I describe myself as a passionate and creative developer
-          who loves coding, design, and the web platform. <br /> <br /> Aside
-          from my job, I always strive to learn more and expand my knowledge in
-          developing. I often find myself spending way too much time watching
-          videos and reading about new smart solutions and projects. <br />
-          <br /> In my free time you can find me longboarding, traveling, in
-          some local pub watching football or on tech meetups and conferences
-          around Gothenburg.
+        <div className="about-me-text-container">
+          <p>
+            Hi! I'm William! <br /> <br /> I'm a Front End developer based in
+            Gothenburg. I describe myself as a passionate and creative developer
+            who loves coding, design, and the web platform. <br /> <br /> Aside
+            from my job, I always strive to learn more and expand my knowledge
+            in developing. I often find myself spending way too much time
+            watching videos and reading about new smart solutions and projects.{" "}
+            <br />
+            <br /> In my free time you can find me longboarding, traveling, in
+            some local pub watching football or on tech meetups and conferences
+            around Gothenburg.
+          </p>
         </div>
       </div>
-      <div>circles</div>
+      <div className="cloud-container">
+        <div class="cloud">
+          <div className="cloud-circle" />
+          <div className="cloud-circle2" />
+        </div>
+      </div>
+      <div className="bottom-button">{">>"}</div>
     </div>
   );
 }
