f96b224a15b864d676a0a6323a4d5afe3ba81692
William Saar <williamsaar@outlook.com>
2022-04-02 21:13:18 +0200

    updated reusable card and added a project

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,45 +4,78 @@ import { ReactComponent as TSLogo } from "../assets/icons/typescript-svgrepo-com
 import { ReactComponent as GithubLogo } from "../assets/icons/github-svgrepo-com.svg";
 import { ReactComponent as LinkLogo } from "../assets/icons/link-svgrepo-com.svg";
 import { ReactComponent as MuiLogo } from "../assets/icons/material-ui-svgrepo-com.svg";
+import { ReactComponent as FigmaLogo } from "../assets/icons/figma-svgrepo-com.svg";
+import { ReactComponent as P5Logo } from "../assets/icons/p5js.svg";
+
+import Tilt from "react-parallax-tilt";
 
 import ReadMore from "./ReadMore";
 import HoverText from "./HoverText";
 
-const ProjectCard = ({ title, gif, text, repo, demo }) => {
+const ProjectCard = ({
+  title,
+  gif,
+  text,
+  repo,
+  demo,
+  react,
+  ts,
+  mui,
+  figma,
+  p5,
+}) => {
   return (
-    <div className="project-card">
-      <h2 style={{ letterSpacing: ".05rem" }}>{title}</h2>
-      <div className="preview-wrapper">
-        <img
-          className="project-preview"
-          src={require(`../assets/imgs/${gif}`)}
-          alt={title}
-        />
+    <Tilt tiltReverse={true} tiltMaxAngleX="5" tiltMaxAngleY="5">
+      <div className="project-card">
+        <h2 style={{ letterSpacing: ".05rem" }}>{title}</h2>
+        <div className="preview-wrapper">
+          <img
+            className="project-preview"
+            src={require(`../assets/imgs/${gif}`)}
+            alt={title}
+          />
+        </div>
+        <span>
+          <ReadMore>{text}</ReadMore>
+        </span>
+        <div style={{ display: "flex", alignItems: "center" }}>
+          Created using:
+          {react ? (
+            <HoverText text="React">
+              <ReactLogo className="project-logo" />
+            </HoverText>
+          ) : null}
+          {ts ? (
+            <HoverText text="Typescript">
+              <TSLogo className="project-logo" />
+            </HoverText>
+          ) : null}
+          {mui ? (
+            <HoverText text="Material UI">
+              <MuiLogo className="project-logo" />
+            </HoverText>
+          ) : null}
+          {figma ? (
+            <HoverText text="Figma">
+              <FigmaLogo className="project-logo" />
+            </HoverText>
+          ) : null}
+          {p5 ? (
+            <HoverText text="P5js">
+              <P5Logo className="project-logo" />
+            </HoverText>
+          ) : null}
+        </div>
+        <div>
+          <a href={repo} target="_blank" rel="noopener noreferrer">
+            <GithubLogo className="project-link" />
+          </a>
+          <a href={demo} target="_blank" rel="noopener noreferrer">
+            <LinkLogo className="project-link" />
+          </a>
+        </div>
       </div>
-      <span>
-        <ReadMore>{text}</ReadMore>
-      </span>
-      <div style={{ display: "flex", alignItems: "center" }}>
-        Created using:
-        <HoverText text="React">
-          <ReactLogo className="project-logo" />
-        </HoverText>
-        <HoverText text="Typescript">
-          <TSLogo className="project-logo" />
-        </HoverText>
-        <HoverText text="Material UI">
-          <MuiLogo className="project-logo" />
-        </HoverText>
-      </div>
-      <div>
-        <a href={repo} target="_blank" rel="noopener noreferrer">
-          <GithubLogo className="project-link" />
-        </a>
-        <a href={demo} target="_blank" rel="noopener noreferrer">
-          <LinkLogo className="project-link" />
-        </a>
-      </div>
-    </div>
+    </Tilt>
   );
 };
 
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -16,6 +16,18 @@ export default function Projects() {
           where we can change products according to the CRUD principle."
           repo="https://github.com/Willen17/miniprojekt2-webbshop"
           demo="https://kameraproffset.netlify.app/"
+          react={true}
+          ts={true}
+          mui={true}
+        />
+        <ProjectCard
+          title="Space Patrol"
+          gif="spacepatrol.gif"
+          text="A game I created with three classmates using the p5js library. The game takes place in space where dangerous asteroids are threatening the planet. You will be positioned in a spacecraft. Your weapon is a laserbeam. Save our planet by shooting down asteroids with the laser."
+          repo="https://github.com/Willen17/Space-Patrol-Game"
+          demo="https://spacepatrol.netlify.app/"
+          p5={true}
+          ts={true}
         />
       </div>
     </div>
